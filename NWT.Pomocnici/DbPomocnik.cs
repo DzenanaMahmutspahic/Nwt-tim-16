769964d753ef289e42bebc4f5cd1d269da8376a9
hjavoras <miniprogrammerme@gmail.com>
2015-03-22 14:47:33 +0100

    Ponovni komit za pomocne klase

---
+++ NWT.Pomocnici/DbPomocnik.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWT.Pomocnici{
+	public class DbPomocnik {
+
+        private static readonly string CONNECTION_STRING = @"Data Source=.\SQLSERVER;Initial Catalog=NWT;Integrated Security=True";
+
+        /// <summary>
+        /// Metoda za izvršavanje procedure na Bazi podataka
+        /// </summary>
+        /// <param name="NazivProcedure"></param>
+        /// <param name="parametri"></param>
+        /// <returns></returns>
+		public static DataTable IzvrsiProceduru(string NazivProcedure, Dictionary<string, object> parametri) {
+			using(SqlConnection conn = new SqlConnection(CONNECTION_STRING)) {
+				conn.Open();
+
+				SqlCommand cmd = new SqlCommand(NazivProcedure, conn);
+
+				cmd.CommandType = CommandType.StoredProcedure;
+
+				if(parametri != null)
+					foreach(KeyValuePair<string, object> parametar in parametri) {
+						cmd.Parameters.Add(new SqlParameter("@" + parametar.Key, parametar.Value));
+					}
+
+				DataTable odgovor = new DataTable();
+				using(SqlDataReader rdr = cmd.ExecuteReader()) {
+					while(rdr.Read()) {
+						List<object> parametri1 = new List<object>();
+						for(int i = 0; i < rdr.FieldCount; i++) {
+							if(!odgovor.Columns.Contains(rdr.GetName(i)))
+								odgovor.Columns.Add(rdr.GetName(i));
+							parametri1.Add(rdr.GetValue(i));
+						}
+						odgovor.Rows.Add(parametri1.ToArray());
+					}
+				}
+				return odgovor;
+			}
+		}
+	}
+}
--- NWT_projekat/Controllers/AccountController.cs
+++ NWT_projekat/Controllers/AccountController.cs
@@ -1,22 +1,27 @@
-﻿using NWT_projekat.Models;
+﻿using NWT.Pomocnici;
+using NWT_projekat.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace NWT_projekat.Controllers {
     public class AccountController: ApiController {
-        
+
         /* [HttpGet]
         public Account Get(int? id=null) {
             return new Account { ID = id??0, Username = "dmahmutspahic", Datum = DateTime.Now };
         }*/
 
-        public Korisnik Get(int? ID=null) {
+        public Korisnik Get(int? ID = null) {
             var K = new Korisnik() { Ime = "Dženana", Prezime = "Mahmutspahić", Pozicija = "Šefica" };
             return K;
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public bool Login(Korisnik korisnik) {
+            return DbPomocnik.IzvrsiProceduru("Login", new Dictionary<string, object>()).Rows.Count == 1;
         }
 
     }
