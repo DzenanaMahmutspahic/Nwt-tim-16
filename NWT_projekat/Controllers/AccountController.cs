259d3c822ea04e49393f99a2ca45700fdd975187
hjavoras <miniprogrammerme@gmail.com>
2015-03-22 11:08:49 +0100

    Inicijalni komit

---
+++ NWT_projekat/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using NWT_projekat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NWT_projekat.Controllers {
+    public class AccountController: Controller {
+        //
+        // GET: /Account/
+
+        public ActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public Account Get(int? id=null) {
+            return new Account { ID = id??0, Username = "dmahmutspahic", Datum = DateTime.Now };
+        }
+
+        /*[HttpGet]
+        public Korisnik Get(int ID) {
+            var K = new Korisnik() { Ime = "Dženana", Prezime = "Mahmutspahić", Pozicija = "Šefica" };
+            return K;
+        }*/
+
+    }
+}
