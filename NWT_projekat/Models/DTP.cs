38450ce4c5812259b675b9d9bb6087369c57d980
DzenanaMahmutspahic <dmahmutspa1@etf.unsa.ba>
2015-03-24 15:34:57 +0100

    Klase

---
+++ NWT_projekat/Models/DTP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWT_projekat.Models
+{
+    public class DTP
+    {
+        public int DTP_ID { get; set; }
+        public int Korisnik_ID { get; set; }
+        public string Sofp_materijal { get; set; }
+        public decimal Sofp_sati { get; set; }
+        public decimal Sofp_cijena { get; set; }
+        public string Fotografija_materijal { get; set; }
+        public decimal Fotografija_sati { get; set; }
+        public decimal Fotografija_cijena { get; set; }
+        public decimal Ukupno_sati { get; set; }
+        public decimal Ukupno_cijena { get; set; }
+        public int Status_DTP { get; set; }
+        public DateTime Vrijeme_cekanja { get; set; }
+        public string Komentar { get; set; }
+        public DateTime Vrijeme_pocetka { get; set; }
+        public DateTime Vrijeme_zavrsetka { get; set; }
+    }
+}
---
+++ NWT_projekat/Models/Montaza.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NWT_projekat.Models
+{
+    public class Montaza
+    {
+         public int ID { get; set; }
+         public int Korisnik_ID { get; set; }
+         public string Snimanje_materijal { get; set; }
+         public decimal Snimanje_sati { get; set; }
+         public decimal Snimanje_cijena { get; set; }
+         public string Montaza_materijal { get; set; }
+         public decimal Montaza_sati { get; set; }
+         public decimal Ukupno_sati { get; set; }
+	     public decimal Ukupno_cijena { get; set; }
+         public int Status_montaze { get; set; }
+         public DateTime Vrijeme_cekanja { get; set; }
+         public string Komentar { get; set; }
+       
+      
+    }
+}
