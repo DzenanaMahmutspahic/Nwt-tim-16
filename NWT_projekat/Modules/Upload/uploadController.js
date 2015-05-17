0e44595f6913fe9dfb53378523a85dd27a3da336
rmuhic1 <rmuhic1@etf.unsa.ba>
2015-05-17 12:30:47 +0200

    upload2

--- NWT_projekat/Modules/Upload/uploadController.js
+++ NWT_projekat/Modules/Upload/uploadController.js
@@ -1,32 +1,76 @@
-﻿//app
-//    .controller('UploadFileController', ['$scope', 'Upload', function ($scope, Upload) {
-//    $scope.$watch('files', function () {
-//        $scope.upload($scope.files);
-//    });
-//    $scope.log = '';
+﻿    app
+        .controller('MyCtrl', ['$scope', 'upload', function ($scope, upload) {
+            $scope.$watch('files', function () {
+                $scope.upload($scope.files);
+            });
+            $scope.log = '';
 
-//    $scope.upload = function (files) {
-//        if (files && files.length) {
-//            for (var i = 0; i < files.length; i++) {
-//                var file = files[i];
-//                Upload
-//                    .upload({
-//                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-//                    fields: {
-//                        'username': $scope.username
-//                    },
-//                    file: file
-//                })
-//                   .progress(function (evt) {
-//                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-//                    $scope.log = 'progress: ' + progressPercentage + '% ' +
-//                                evt.config.file.name + '\n' + $scope.log;
-//                   })
-//                    .success(function (data, status, headers, config) {
-//                    $scope.log = 'file ' + config.file.name + 'uploaded. Response: ' + JSON.stringify(data) + '\n' + $scope.log;
-//                    $scope.$apply();
-//                });
-//            }
-//        }
-//    };
-//}]);
+            $scope.upload = function (files) {
+                if (files && files.length) {
+                    for (var i = 0; i < files.length; i++) {
+                        var file = files[i];
+                        upload
+                            .upload({
+                                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                                fields: {
+                                    'username': $scope.username
+                                },
+                                file: file
+                            })
+                           .progress(function (evt) {
+                               var progresspercentage = parseint(100.0 * evt.loaded / evt.total);
+                               $scope.log = 'progress: ' + progresspercentage + '% ' +
+                                           evt.config.file.name + '\n' + $scope.log;
+                           })
+                            .success(function (data, status, headers, config) {
+                                $scope.log = 'file ' + config.file.name + 'uploaded. response: ' + json.stringify(data) + '\n' + $scope.log;
+                                $scope.$apply();
+                            });
+                    }
+                }
+            };
+        }]);
+        //.directive('ngThumb', ['$window', function ($window) {
+        //    var helper = {
+        //        support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+        //        isFile: function (item) {
+        //            return angular.isObject(item) && item instanceof $window.File;
+        //        },
+        //        isImage: function (file) {
+        //            var type = '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+        //            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        //        }
+        //    };
+
+        //    return {
+        //        restrict: 'A',
+        //        template: '<canvas/>',
+        //        link: function (scope, element, attributes) {
+        //            if (!helper.support) return;
+
+        //            var params = scope.$eval(attributes.ngThumb);
+
+        //            if (!helper.isFile(params.file)) return;
+        //            if (!helper.isImage(params.file)) return;
+
+        //            var canvas = element.find('canvas');
+        //            var reader = new FileReader();
+
+        //            reader.onload = onLoadFile;
+        //            reader.readAsDataURL(params.file);
+
+        //            function onLoadFile(event) {
+        //                var img = new Image();
+        //                img.onload = onLoadImage;
+        //                img.src = event.target.result;
+        //            }
+
+        //            function onLoadImage() {
+        //                var width = params.width || this.width / this.height * params.height;
+        //                var height = params.height || this.height / this.width * params.width;
+        //                canvas.attr({ width: width, height: height });
+        //                canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+        //            }
+        //        }
+        //    };
+        //}]);
