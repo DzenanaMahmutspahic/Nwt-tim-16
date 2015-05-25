e041622f4749a3926a0665b7be5f59ea9902daae
DzenanaMahmutspahic <dmahmutspa1@etf.unsa.ba>
2015-05-26 01:25:16 +0200

    Upload slike dorađeno

--- NWT_projekat/Modules/Upload/uploadController.js
+++ NWT_projekat/Modules/Upload/uploadController.js
@@ -1,35 +1,35 @@
-﻿    app
-        .controller('MyCtrl', ['$scope', 'upload', function ($scope, upload) {
-            $scope.$watch('files', function () {
-                $scope.upload($scope.files);
-            });
-            $scope.log = '';
+﻿    //app
+    //    .controller('MyCtrl', ['$scope', 'upload', function ($scope, upload) {
+    //        $scope.$watch('files', function () {
+    //            $scope.upload($scope.files);
+    //        });
+    //        $scope.log = '';
 
-            $scope.upload = function (files) {
-                if (files && files.length) {
-                    for (var i = 0; i < files.length; i++) {
-                        var file = files[i];
-                        upload
-                            .upload({
-                                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                                fields: {
-                                    'username': $scope.username
-                                },
-                                file: file
-                            })
-                           .progress(function (evt) {
-                               var progresspercentage = parseint(100.0 * evt.loaded / evt.total);
-                               $scope.log = 'progress: ' + progresspercentage + '% ' +
-                                           evt.config.file.name + '\n' + $scope.log;
-                           })
-                            .success(function (data, status, headers, config) {
-                                $scope.log = 'file ' + config.file.name + 'uploaded. response: ' + json.stringify(data) + '\n' + $scope.log;
-                                $scope.$apply();
-                            });
-                    }
-                }
-            };
-        }]);
+    //        $scope.upload = function (files) {
+    //            if (files && files.length) {
+    //                for (var i = 0; i < files.length; i++) {
+    //                    var file = files[i];
+    //                    upload
+    //                        .upload({
+    //                            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+    //                            fields: {
+    //                                'username': $scope.username
+    //                            },
+    //                            file: file
+    //                        })
+    //                       .progress(function (evt) {
+    //                           var progresspercentage = parseint(100.0 * evt.loaded / evt.total);
+    //                           $scope.log = 'progress: ' + progresspercentage + '% ' +
+    //                                       evt.config.file.name + '\n' + $scope.log;
+    //                       })
+    //                        .success(function (data, status, headers, config) {
+    //                            $scope.log = 'file ' + config.file.name + 'uploaded. response: ' + json.stringify(data) + '\n' + $scope.log;
+    //                            $scope.$apply();
+    //                        });
+    //                }
+    //            }
+    //        };
+    //    }]);
         //.directive('ngThumb', ['$window', function ($window) {
         //    var helper = {
         //        support: !!($window.FileReader && $window.CanvasRenderingContext2D),
@@ -73,4 +73,63 @@
         //            }
         //        }
         //    };
-        //}]);
+//}]);
+
+
+
+    (function (module) {
+
+        var fileReader = function ($q, $log) {
+
+            var onLoad = function (reader, deferred, scope) {
+                return function () {
+                    scope.$apply(function () {
+                        deferred.resolve(reader.result);
+                    });
+                };
+            };
+
+            var onError = function (reader, deferred, scope) {
+                return function () {
+                    scope.$apply(function () {
+                        deferred.reject(reader.result);
+                    });
+                };
+            };
+
+            var onProgress = function (reader, scope) {
+                return function (event) {
+                    scope.$broadcast("fileProgress",
+                        {
+                            total: event.total,
+                            loaded: event.loaded
+                        });
+                };
+            };
+
+            var getReader = function (deferred, scope) {
+                var reader = new FileReader();
+                reader.onload = onLoad(reader, deferred, scope);
+                reader.onerror = onError(reader, deferred, scope);
+                reader.onprogress = onProgress(reader, scope);
+                return reader;
+            };
+
+            var readAsDataURL = function (file, scope) {
+                var deferred = $q.defer();
+
+                var reader = getReader(deferred, scope);
+                reader.readAsDataURL(file);
+
+                return deferred.promise;
+            };
+
+            return {
+                readAsDataUrl: readAsDataURL
+            };
+        };
+
+        module.factory("fileReader",
+                       ["$q", "$log", fileReader]);
+
+    }(angular.module("BasicHttpAuthExample")));
