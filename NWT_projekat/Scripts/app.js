f6241831e76feb26f3b9e6c176c77360d9d2007c
DzenanaMahmutspahic <dmahmutspa1@etf.unsa.ba>
2015-05-26 00:23:51 +0200

    Upload file-a

--- NWT_projekat/Scripts/app.js
+++ NWT_projekat/Scripts/app.js
@@ -14,6 +14,9 @@ var app = angular.module('BasicHttpAuthExample', [
     'pascalprecht.translate',
     'ngRoute',
     'ngCookies'
+ //   'angularFileUpload'
+
+    
 ])
 
 
@@ -165,39 +168,56 @@ app.controller('RegistrationController',
                 }
             });
         };
-    }]);
-app
-    .controller('UploadFileController', ['$scope', 'Upload', function ($scope, Upload) {
-        $scope.$watch('files', function () {
-            $scope.upload($scope.files);
-        });
-        $scope.log = '';
+    }])
 
-        $scope.upload = function (files) {
-            if (files && files.length) {
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i];
-                    Upload
-                        .upload({
-                            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                            fields: {
-                                'username': $scope.username
-                            },
-                            file: file
-                        })
-                       .progress(function (evt) {
-                           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                           $scope.log = 'progress: ' + progressPercentage + '% ' +
-                                       evt.config.file.name + '\n' + $scope.log;
-                       })
-                        .success(function (data, status, headers, config) {
-                            $scope.log = 'file ' + config.file.name + 'uploaded. Response: ' + JSON.stringify(data) + '\n' + $scope.log;
-                            $scope.$apply();
-                        });
-                }
-            }
-        };
-    }]);
+
+//app.controller('UploadFileController', ['$scope', $upload, function ($scope, $upload) {
+//        $scope.$watch('files', function () {
+//            $scope.upload($scope.files);
+//        });
+//        $scope.log = '';
+
+//        $scope.upload = function (files) {
+//            if (files && files.length) {
+//                for (var i = 0; i < files.length; i++) {
+//                    var file = files[i];
+//                    Upload
+//                        .upload({
+//                            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+//                            fields: {
+//                                'username': $scope.username
+//                            },
+//                            file: file
+//                        })
+//                       .progress(function (evt) {
+//                           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+//                           $scope.log = 'progress: ' + progressPercentage + '% ' +
+//                                       evt.config.file.name + '\n' + $scope.log;
+//                       })
+//                        .success(function (data, status, headers, config) {
+//                            $scope.log = 'file ' + config.file.name + 'uploaded. Response: ' + JSON.stringify(data) + '\n' + $scope.log;
+//                            $scope.$apply();
+//                        });
+//                }
+//            }
+//        };
+//}]);
+
+.controller('UploadFileController', ['$scope', '$http', function ($scope, $http) {
+    //var myModelObj = {};
+
+    $scope.onFileSelect = function ($files, myModelObj) {
+        $http.uploadFile({
+            url: 'D:\\',
+            file: $file[0] // for single file
+            //files: $files  // for multiple files
+        }).then(function (data) {
+            myModelObj.fileId = data;
+        });
+        //return myModelObj;
+    }
+}]);
+
 app.controller('PosaoController', []);
 app.factory('AuthenticationService',
     ['Base64', '$http', '$cookieStore', '$rootScope', '$timeout',
